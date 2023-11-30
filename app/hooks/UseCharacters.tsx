5f51da5e5f2bc92ee48c6d8a93e6364079f63787
juanichaide <jie_chaide@hotmail.com>
2023-11-30 11:36:35 -0300

    zustand initial implementation

--- app/hooks/UseCharacters.tsx
+++ app/hooks/UseCharacters.tsx
@@ -1,11 +1,20 @@
 import {useQuery} from '@tanstack/react-query';
 import {getAllPeople} from '../services';
+import {useCharacterStore} from '../store';
+import {useEffect} from 'react';
 
 const useCharacterData = () => {
-  const {data: characters, status} = useQuery({
+  const {characters, setCharacters} = useCharacterStore();
+  const {data: charactersData, status} = useQuery({
     queryKey: ['characters'],
     queryFn: getAllPeople,
   });
+
+  useEffect(() => {
+    if (charactersData?.results?.length) {
+      setCharacters(charactersData?.results);
+    }
+  }, [charactersData, setCharacters]);
 
   return {characters, status};
 };
